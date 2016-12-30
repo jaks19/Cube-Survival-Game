b858678e5063a385ffd027a63aba990c8af69afb
Adarsh <jaks19@mit.edu>
2016-12-30 07:33:20 -0500

    Spike traps, Smooth respwaning, Prefabs optimized, Prince and Princess drawn

---
+++ Assets/Scripts/TutorialTypeScripts/TutTrap.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Protrude());
+	}
+
+	// Co-routine for spike protrusion
+	IEnumerator Protrude(){
+		while (true) {
+			GetComponent<Animation> ().Play();
+			yield return new WaitForSeconds (3.0f);
+		}
+	}
+}
